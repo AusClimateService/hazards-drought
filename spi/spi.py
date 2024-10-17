79b3c14601e467c71a5916e79f2e98dcdeb11c72
jb6465 <jessica.bhardwaj@bom.gov.au>
2024-10-17 16:19:58 +1100

    updates to SPI/percentiles

--- spi/spi.py
+++ spi/spi.py
@@ -100,40 +100,40 @@ def main(inargs):
     print("Dask dashboard is available at:", client.dashboard_link)
 
     RCM = inargs.RCM
-    #MPI-ESM1-2-HR // NorESM2-MM for BARPA only and CNRM-ESM2-1 for CCAM only
-    model_list = ['CMCC-ESM2', 'ACCESS-ESM1-5', 'ACCESS-CM2', 'EC-Earth3', 'CESM2'] 
-    model_list = model_list + ['CNRM-ESM2-1'] if RCM == 'CCAM-v2203-SN' else model_list + ['MPI-ESM1-2-HR', 'NorESM2-MM']
-    
-    # Compute SPI for each model
-    for model in model_list:
-        print('========= '+RCM+'_'+model+' =========')
-        bc_string = '_ACS-{}-{}-{}-{}.nc'.format(inargs.bcMethod, inargs.bcSource, '1960' if inargs.bcSource == 'AGCD' else '1979', '2022') if inargs.bc == 'output' else '.nc'
-        variant_id = utils.data_source['CMIP6'][model]['variant-id']
-        file_name = "{}/SPI{}_{}_{}_{}_{}_{}_{}_{}{}".format(inargs.outputDir, inargs.spiAccumulation,'AGCD-05i',model,'ssp370',variant_id,'BOM' if RCM == 'BARPA-R' else 'CSIRO','v1-r1','baseperiod'+(str(inargs.basePeriodStart)+str(inargs.basePeriodEnd)),'_raw.nc' if inargs.bc == 'raw' else bc_string)
         
-        if os.path.exists(file_name)==False:
-            print("Computing {name}...".format(name=file_name))
+    # Compute SPI for input GCM/RCM/bc file
+    GCM = inargs.GCM
+    print('========= '+RCM+'_'+GCM+' =========')
+    bc_string = '_ACS-{}-{}-{}-{}.nc'.format(inargs.bcMethod, inargs.bcSource, '1960' if inargs.bcSource == 'AGCD' else '1979', '2022') if inargs.bc == 'output' else '.nc'
+    variant_id = utils.data_source['CMIP6'][GCM]['variant-id']
+    file_name = "{}/SPI{}_{}_{}_{}_{}_{}_{}_{}{}".format(inargs.outputDir, inargs.spiAccumulation,'AGCD-05i',GCM,'ssp370',variant_id,'BOM' if RCM == 'BARPA-R' else 'CSIRO','v1-r1','baseperiod'+(str(inargs.basePeriodStart)+str(inargs.basePeriodEnd)),'_raw.nc' if inargs.bc == 'raw' else bc_string)
+    
+    if os.path.exists(file_name)==False:
+        print("Computing {name}...".format(name=file_name))
 
-            # Group by month and apply the SPI calculation
-            input_array = utils.load_target_variable('var_p', RCM, model, inargs.spiAccumulation, bc=inargs.bc, bc_method=inargs.bcMethod, bc_source=inargs.bcSource)
-            input_array = input_array.astype(np.float32).chunk({'time':-1, 'lat':'auto', 'lon':'auto'})
-            spi_array_grouped = input_array.groupby('time.month').map(lambda x: compute_spi(x, inargs.basePeriodStart, inargs.basePeriodEnd))
-            
-            ds_SPI = spi_array_grouped.assign_coords(time=input_array['time'])
-            ds_SPI = ds_SPI.rename('SPI{}'.format(inargs.spiAccumulation))
+        # Group by month and apply the SPI calculation
+        input_array = utils.load_target_variable('var_p', RCM, GCM, inargs.spiAccumulation, bc=inargs.bc, bc_method=inargs.bcMethod, bc_source=inargs.bcSource)
+        input_array = input_array.astype(np.float32).chunk({'time':-1, 'lat':'auto', 'lon':'auto'})
+        spi_array_grouped = input_array.groupby('time.month').map(lambda x: compute_spi(x, inargs.basePeriodStart, inargs.basePeriodEnd))
+        
+        ds_SPI = spi_array_grouped.assign_coords(time=input_array['time'])
+        ds_SPI = ds_SPI.rename('SPI{}'.format(inargs.spiAccumulation))
 
-            ds_SPI.attrs['description'] = "Standardised Precipitation Index computed using method of McKee et al. 1993 using a base period of {}-{}. Further details in supporting technical documentation.".format(inargs.basePeriodStart, inargs.basePeriodEnd)
-            ds_SPI.attrs['created'] = (datetime.now()).strftime("%d/%m/%Y %H:%M:%S")    
-            ds_SPI.attrs['history'] = cmdprov.new_log(extra_notes=[get_git_hash()])
+        ds_SPI.attrs['description'] = "Standardised Precipitation Index computed using method of McKee et al. 1993 using a base period of {}-{}. Further details in supporting technical documentation.".format(inargs.basePeriodStart, inargs.basePeriodEnd)
+        ds_SPI.attrs['created'] = (datetime.now()).strftime("%d/%m/%Y %H:%M:%S")    
+        ds_SPI.attrs['history'] = cmdprov.new_log(extra_notes=[get_git_hash()])
+        ds_SPI.attrs['driving GCM'] = GCM
+        ds_SPI.attrs['variant ID'] = variant_id
+        ds_SPI.attrs['RCM'] = RCM
 
-            # Specify compression options to avoid large file sizes
-            encoding = {'SPI{}'.format(inargs.spiAccumulation): {'zlib': True, 'complevel': 5, 'dtype':'float32'}}
-            
-            # Save output
-            ds_SPI.to_netcdf(file_name, encoding=encoding)
-            
-        else:
-            print("{name} exists. Pass.".format(name=file_name))
+        # Specify compression options to avoid large file sizes
+        encoding = {'SPI{}'.format(inargs.spiAccumulation): {'zlib': True, 'complevel': 5, 'dtype':'float32'}}
+        
+        # Save output
+        ds_SPI.to_netcdf(file_name, encoding=encoding)
+        
+    else:
+        print("{name} exists. Pass.".format(name=file_name))
 
     #< Close the client
     client.close()
@@ -155,9 +155,10 @@ author:
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
                                      
     parser.add_argument("--RCM", type=str, choices=['BARPA-R', 'CCAM-v2203-SN'], help="Choose RCM from ['BARPA-R', 'CCAM-v2203-SN']")
+    parser.add_argument("--GCM", type=str, choices=['CMCC-ESM2', 'ACCESS-ESM1-5', 'ACCESS-CM2', 'EC-Earth3', 'CESM2', 'CNRM-ESM2-1', 'MPI-ESM1-2-HR', 'NorESM2-MM'], help="Input GCM - check ia39 for correct GCM/RCM availabilies")
     parser.add_argument("--bc", type=str, choices=['raw','input','output'], help="Choose either 'raw' (py18/hq89 raw BARPA/CCAM), 'input' (ia39 bc input, 5km) or 'output' (ia39 bc output, 5km).")
     parser.add_argument("--bcSource", type=str, default='AGCD', choices=['AGCD', 'BARRA'], help="Choose either 'AGCD', 'BARRA'. Default is 'AGCD'")
-    parser.add_argument("--bcMethod", type=str, default='QME', choices=['QME', 'MRNBE'], help="Choose either 'MRNBC', 'QME'. Default is 'QME'")
+    parser.add_argument("--bcMethod", type=str, default='QME', choices=['QME', 'MRNBC'], help="Choose either 'MRNBC', 'QME'. Default is 'QME'")
     parser.add_argument("--spiAccumulation", type=int, default=3, help="Choose SPI accumulation i.e. SPI-1, SPI-3, SPI-6, SPI-12. Default is 3")
     parser.add_argument("--basePeriodStart", type=int, default=1965, help="Specify start year for base period. Minimum 30 years advised. 50 years helps avoid NaN errors on upper tails. Default = 1965.")
     parser.add_argument("--basePeriodEnd", type=int, default=2014, help="Specify end year for base period. Minimum 30 years advised. 50 years helps avoid NaN errors on upper tails. Default = 2014")
